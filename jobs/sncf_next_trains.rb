336440114de5440e9c502f5055ecec8a24381e87
Manfred Touron <m@42.am>
2016-05-20 20:04:55 +0200

    Fix ruby exception

--- jobs/sncf_next_trains.rb
+++ jobs/sncf_next_trains.rb
@@ -1,12 +1,15 @@
-# encoding: utf-8
 require 'open-uri'
 
 SCHEDULER.every '15s', :first_in => 0 do |job|
   response = Net::HTTP.get_response("www.gares-sncf.com", "/fr/train-times/departure/RRD/gl")
   if response.code == "200"
-    result = JSON.parse(response.body)
-    trains = result["trains"]
-    send_event('sncf_next_trains', {sncf_next_trains: trains[0..8]})
+    if response.body != '""'
+      result = JSON.parse(response.body)
+      trains = result["trains"]
+      send_event("sncf_next_trains", {sncf_next_trains: trains[0..8]})
+    else
+      send_event("sncf_next_trains", {sncf_net_trains: []})
+    end
   else
     puts "SNCF ERROR"
   end
